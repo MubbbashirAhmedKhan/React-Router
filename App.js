1e5f945f7e36f3bb52b8648600b2131f2aeeb6e0
MubbbashirAhmedKhan <76056522+MubbbashirAhmedKhan@users.noreply.github.com>
2021-06-24 01:23:31 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import RouterApp from './config/router';
+
+
+class App extends Component{
+  render(){
+    return(
+      
+      < RouterApp />
+    )
+  }
+}
+
+
+
+
+export default App;
